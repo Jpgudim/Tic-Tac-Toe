8122c70e44f763f8314b691720e4d03e3e1fe1e9
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-06-27 17:19:15 -0500

    Add files via upload
    Made some changes:
    - added code to address if player chooses a square that is already taken
    - added code to address if player chooses coordinates that are out of bounds
    - other minor adjustments to make sure things run smoothly with new code

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -6,7 +6,7 @@ for x in range(0,3):
     board.append(["[ ]"] * 3)
 
 print ()
-print ("Welcom to Tic Tac Toe! Here is the game board, with numbers representing each sqaure")
+print ("Welcome to Tic Tac Toe! Here is the game board, with numbers representing each sqaure")
 print ()
 
 def get_board(board):
@@ -15,18 +15,42 @@ def get_board(board):
         print (" ".join(row))
     print ()
     
-get_board(board)
-print ()
 print ("Player 1, you are X's and player 2, you are O's")
 
 for turn in range(9):
     turn + 1
     print ("Player " + str((turn % 2) + 1) + ", your turn!")
+    get_board(board)
     row_select = int(input("Select Row: "))
     col_select = int(input("Select Col: "))
 
-    if board[row_select][col_select] == "[X]":
+    if  row_select not in range(4) or col_select not in range(4):
+        print()
+        print ("That's not on the board! Try again player " + str((turn % 2) + 1))
+        get_board(board)
+        row_select = int(input("Select Row: "))
+        col_select = int(input("Select Col: "))
+        if (turn % 2) == 0:
+            board[row_select - 1][col_select - 1] = "[X]"
+            get_board(board)
+        else:
+            board[row_select - 1][col_select - 1] = "[O]"
+            get_board(board)
+    elif board[row_select - 1][col_select - 1] == "[X]" or board[row_select - 1][col_select - 1] == "[O]":
+        print ()
         print ("That spot has already been taken!")
+        print ()
+        print ("Player " + str((turn % 2) + 1) + ", go again!")
+        get_board(board)
+        row_select = int(input("Select Row: "))
+        col_select = int(input("Select Col: "))
+        if (turn % 2) == 0:
+            board[row_select - 1][col_select - 1] = "[X]"
+            get_board(board)
+        else:
+            board[row_select - 1][col_select - 1] = "[O]"
+            get_board(board)
+        get_board(board)    
     else:
         if (turn % 2) == 0:
             board[row_select - 1][col_select - 1] = "[X]"
@@ -36,7 +60,19 @@ for turn in range(9):
             get_board(board)
 
     
+#scenarios I need to work on:
+#1. input is not an int
+#       simple if statement, just need to figure out where to put it
+#2. player wins the game (3 x's or 3 o's in a line)
+#       maybe something like " if board[row] or board[col] = 3 of same letters"
+#       this doesnt account for diagonal line though
+#3. no longer possible for a player to win the game
+#       no idea how I will do this
 
+#potential issues with code:
+#1. not finished
+#2. not sure yet how im going to finish it
+#3. lot of repeating if/else scenarios. I feel like it could be optimized and cleaned
 
 
 
