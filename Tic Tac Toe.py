3ffed590177f35681fc45a91116d3f51c2870a5e
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-06-26 21:35:27 -0500

    Add files via upload
    Made some changes after learning some things in CodeAcademy

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,55 +1,45 @@
 # This is my first attempt at making tic-tac-toe in python entirely from scratch
 # I'm not exactly sure how to do it, so i'm going to figure it out as I go
 
-row_1 = ["|   |   |   |"]
-row_2 = ["|   |   |   |"]
-row_3 = ["|   |   |   |"]
+board = []
+for x in range(0,3):
+    board.append(["[ ]"] * 3)
 
 print ()
 print ("Welcom to Tic Tac Toe! Here is the game board, with numbers representing each sqaure")
 print ()
-print ("_____________")
-print ("| 1 | 2 | 3 |")
-print ("-------------")
-print ("| 4 | 5 | 6 |")
-print ("-------------")
-print ("| 7 | 8 | 9 |")
-print ("-------------")
-print ()
 
-def get_board():
+def get_board(board):
     print ()
-    print (row_1)
-    print (row_2)
-    print (row_3)
+    for row in board:
+        print (" ".join(row))
     print ()
     
-
+get_board(board)
+print ()
 print ("Player 1, you are X's and player 2, you are O's")
-turn_1 = input ("Player 1, you're up! Which square do you choose? ")
-if turn_1 == "1":
-    row_1 = ["| X |   |   |"]
-elif turn_1 == "2":
-    row_1 = ["|   | X |   |"]
-elif turn_1 == "3":
-    row_1 = ["|   |   | X |"]
-elif turn_1 == "4":
-    row_2 = ["| X |   |   |"]
-elif turn_1 == "5":
-    row_2 = ["|   | X |   |"]
-elif turn_1 == "6":
-    row_2 = ["|   |   | X |"]
-elif turn_1 == "7":
-    row_3 = ["| X |   |   |"]
-elif turn_1 == "8":
-    row_3 = ["|   | X |   |"]
-elif turn_1 == "9":
-    row_3 = ["|   |   | X |"]
 
-get_board()
+for turn in range(9):
+    turn + 1
+    print ("Player " + str((turn % 2) + 1) + ", your turn!")
+    row_select = int(input("Select Row: "))
+    col_select = int(input("Select Col: "))
+
+    if board[row_select][col_select] == "[X]":
+        print ("That spot has already been taken!")
+    else:
+        if (turn % 2) == 0:
+            board[row_select - 1][col_select - 1] = "[X]"
+            get_board(board)
+        else:
+            board[row_select - 1][col_select - 1] = "[O]"
+            get_board(board)
+
+    
 
 
-# placeholder for how I will prevent over riding previous selections
-if turn_2 == turn_1:
-    print ("That spot has already been taken! Try again.")
+
+
+
+
 
